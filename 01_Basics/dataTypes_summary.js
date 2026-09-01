f7d80998284102ecfe72b62478419f216a010ddc
Nandani Kumari <nraj21.nandani@gmail.com>
2026-09-01 17:04:13 +0000

    Number&Maths

--- 01_Basics/dataTypes_summary.js
+++ 01_Basics/dataTypes_summary.js
@@ -2,14 +2,14 @@
 
 //7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 1234566789n
 
@@ -17,14 +17,32 @@ console.log(id === anotherId);
 
 //Arrays, Objects,  Functions
 
-const heroes = ["shaktiman",  "doremon", "doga"];
-let myObj = {
-    name: "Nandani",
-    age: 19,
-}
-const myFunction = function(){
-    console.log("Hello I,m Nandani Gupta");
-}
+// const heroes = ["shaktiman",  "doremon", "doga"];
+// let myObj = {
+//     name: "Nandani",
+//     age: 19,
+// }
+// const myFunction = function(){
+//     console.log("Hello I,m Nandani Gupta");
+// }
 
-console.log(typeof heroes);
+// console.log(typeof heroes);
 
+//************* 
+
+//Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "Nandanidotcom"
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+console.log(anothername);
+console.log(myYoutubename);
+
+let user1 = {
+    email: "nandu@.com",
+    upi: "user@ybl"
+}
+let user2 = user1
+user1.email = "nandu@google.com"
+console.log(user1.email);
+console.log(user2.email);
