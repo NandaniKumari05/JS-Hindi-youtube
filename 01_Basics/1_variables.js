fe664ce0b5804979feda8bbd157ece8ccd42c59b
Nandani Kumari <nraj21.nandani@gmail.com>
2026-09-01 07:25:27 +0000

    datatypes

---
+++ 01_Basics/1_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1445533
+let accountEmail = "nandu@gmail.com"
+var_accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed
+accountEmail = "ng@gmail.com"
+accountPassword = "21321"
+accountCity = "Delhi"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
