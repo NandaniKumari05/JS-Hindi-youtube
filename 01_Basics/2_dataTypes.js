f5bf37c4ba2748dc69dfee8afce1f4f0b89d9790
Nandani Kumari <nraj21.nandani@gmail.com>
2026-09-01 07:36:21 +0000

    dataTypes

--- 01_Basics/2_dataTypes.js
+++ 01_Basics/2_dataTypes.js
@@ -2,4 +2,21 @@
 
 //alert(3 + 3)  //we are using node js , not browser
 
+console.log("Nandani")
 
+let name = "Nandu"
+let age = 19
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+    //symbol => unique
+
+//object
+
+console.log(typeof "Nandu"); //undefined
+console.log(typeof null); //object
