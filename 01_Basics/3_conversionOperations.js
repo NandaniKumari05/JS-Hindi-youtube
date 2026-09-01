6f6944604623eed6d8d09e660a9dfe8558b3cb88
Nandani Kumari <nraj21.nandani@gmail.com>
2026-09-01 12:23:12 +0000

    datatype summary

---
+++ 01_Basics/3_conversionOperations.js
@@ -0,0 +1,63 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"Nandu" => true
+
+let SomeNumber = 35
+let stringNumber = String(SomeNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+//*************Operations*************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = "World"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true); //1
+// console.log(+""); //0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
---
+++ 01_Basics/dataTypes_summary.js
@@ -0,0 +1,30 @@
+// # Primitive Datatype
+
+//7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+console.log(id === anotherId);
+
+// const bigNumber = 1234566789n
+
+//Reference (Non Primitive)
+
+//Arrays, Objects,  Functions
+
+const heroes = ["shaktiman",  "doremon", "doga"];
+let myObj = {
+    name: "Nandani",
+    age: 19,
+}
+const myFunction = function(){
+    console.log("Hello I,m Nandani Gupta");
+}
+
+console.log(typeof heroes);
+
