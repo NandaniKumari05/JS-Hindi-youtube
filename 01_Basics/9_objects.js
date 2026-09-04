cad1ad61b3942a397609fb431e1858992df8fbbe
Nandani Kumari <nraj21.nandani@gmail.com>
2026-09-04 12:06:31 +0000

    objects

---
+++ 01_Basics/9_objects.js
@@ -0,0 +1,26 @@
+Part 1
+Object literals
+
+const mySym = Symbol("key1")
+const JsUser = {
+    name: "Nandani",
+    "full name": "Nandani Kuamri",
+    [mySym]: "mykey1",
+    age: 19,
+    location: "delhi",
+    email: "nandu@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+console.log(JsUser.email)
+console.log(JsUser["email"])
+console.log(JsUser["full name"])
+console.log(typeof JsUser[mySym])
+
+JsUser.email = "nandani@.com"
+// Object.freeze(JsUser)
+JsUser.email = "nandaniKri@.com"
+// console.log(JsUser);
+
+
+
