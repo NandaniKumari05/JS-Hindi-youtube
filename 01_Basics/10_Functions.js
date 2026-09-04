28fa1ee441234f3f578c0672f48452fca06ccda4
Nandani Kumari <nraj21.nandani@gmail.com>
2026-09-04 17:08:13 +0000

    functions

---
+++ 01_Basics/10_Functions.js
@@ -0,0 +1,24 @@
+function sayMyName(){
+    console.log("N");
+    console.log("A");
+    console.log("N");
+    console.log("D");
+    console.log("A");
+    console.log("N");
+    console.log("I");
+}
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+const result = addTwoNumbers(4, 5)
+
+ console.log("result" = result);
