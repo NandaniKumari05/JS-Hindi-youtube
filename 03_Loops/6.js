359665a0c192a44f1f9f0e762c1610ed583abf24
Nandani Kumari <nraj21.nandani@gmail.com>
2026-09-12 11:37:54 +0000

    forEach

---
+++ 03_Loops/6.js
@@ -0,0 +1,17 @@
+// const codding = ["js", "python", "rubby", "cpp"]
+
+// const values = codding.forEach((item) => {
+//     console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter((num) => num > 4)
+const newNums = myNums.filter((num) => {
+    num > 4
+})
+
+console.log(newNums);
